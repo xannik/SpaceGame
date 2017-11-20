c5099c105385ecaf03949cb7f53502551984e48e
xannik <rasmus_lindy@hotmail.com>
2017-11-20 23:25:17 +0900

    shooting
    Shooting works

---
+++ PlayerShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject bulletPreFab;
+    public float fireDelay = 0.25f;
+    private float cooldownTimer = 0;
+
+    void Start () {
+
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldownTimer -= Time.deltaTime;
+        Debug.Log(Input.GetButton("Fire1"));
+
+        if(Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            Debug.Log("Pew!");
+            cooldownTimer = fireDelay;
+            Instantiate(bulletPreFab, transform.position, transform.rotation);
+        }
+	}
+}
