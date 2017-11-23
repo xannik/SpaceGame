16c717e8c48b0959ed5d30fb68b198b3b5e1f88d
Melvin MOHADEB <melvin.mohadeb@epitech.eu>
2017-11-24 01:57:31 +0900

    Enemy is avoiding obstacles

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -8,6 +8,9 @@ public class EnemyAI : MonoBehaviour {
     public float speed = 10f;
     public float turnspeed = 1.5f;
     public float fireRate = 0.5F;
+    public float rayCastOffset = 10f;
+    public float detectionDistance = 100f;
+
     private float nextFire = 0.0F;
 
     void ShootPlayer()
@@ -37,8 +40,41 @@ public class EnemyAI : MonoBehaviour {
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 
+    void pathFinding()
+    {
+        RaycastHit hit;
+        Vector3 raycastOffset = Vector3.zero;
+
+        Vector3 left = transform.position - transform.right * rayCastOffset;
+        Vector3 right = transform.position + transform.right * rayCastOffset;
+        Vector3 up = transform.position + transform.up * rayCastOffset;
+        Vector3 down = transform.position - transform.up * rayCastOffset;
+
+        Debug.DrawRay(left, transform.forward * detectionDistance, Color.cyan);
+        Debug.DrawRay(right, transform.forward * detectionDistance, Color.cyan);
+        Debug.DrawRay(up, transform.forward * detectionDistance, Color.cyan);
+        Debug.DrawRay(down, transform.forward * detectionDistance, Color.cyan);
+
+        if (Physics.Raycast(left, transform.forward, out hit, detectionDistance))
+            raycastOffset += Vector3.right;
+        else if(Physics.Raycast(right, transform.forward, out hit, detectionDistance))
+            raycastOffset -= Vector3.right;
+
+        if (Physics.Raycast(up, transform.forward, out hit, detectionDistance))
+            raycastOffset += Vector3.up;
+        else if (Physics.Raycast(down, transform.forward, out hit, detectionDistance))
+            raycastOffset -= Vector3.up;
+
+        if (raycastOffset != Vector3.zero)
+            transform.Rotate(raycastOffset * 10f * Time.deltaTime);
+        else
+            rotateToPlayer();
+
+    }
+
     void Update() {
-        rotateToPlayer();
+        //rotateToPlayer();
+        pathFinding();
         moveForward();
         ShootPlayer();
     }
