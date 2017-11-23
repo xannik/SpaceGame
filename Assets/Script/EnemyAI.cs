c16952af10b1f96c3a2425bc4ba804cbf9f9be01
Melvin MOHADEB <melvin.mohadeb@epitech.eu>
2017-11-23 17:01:48 +0900

    Improve Enemy movement

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour {
     public GameObject player;       //Public variable to store a reference to the player game object
     public float speed;
+    public float turnspeed;
 
-    void FixedUpdate() {
-        transform.LookAt(player.transform);
+    void Update() {
+        Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnspeed * Time.deltaTime);
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 }
