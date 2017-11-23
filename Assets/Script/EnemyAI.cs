bced674dbe17acaab5c588a92bcfa9e22b68682c
Melvin MOHADEB <melvin.mohadeb@epitech.eu>
2017-11-23 23:55:16 +0900

    Enemy shoot + planets + player velocity modification

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
     public GameObject player;       //Public variable to store a reference to the player game object
-    public float speed;
-    public float turnspeed;
+    public GameObject shotPrefab;
+    public float speed = 10f;
+    public float turnspeed = 1.5f;
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
 
-    void Update() {
+    void ShootPlayer()
+    {
+        Vector3 relativePoint = transform.InverseTransformPoint(player.transform.position);
+
+        Debug.Log(relativePoint.x);
+
+        if (relativePoint.x < 5.0 &&
+            relativePoint.x > -5.0 &&
+            Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject go = GameObject.Instantiate(shotPrefab, transform.position, transform.rotation) as GameObject;
+            GameObject.Destroy(go, 3f);
+        }
+    }
+
+    void rotateToPlayer()
+    {
         Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnspeed * Time.deltaTime);
+    }
+
+    void moveForward()
+    {
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    void Update() {
+        rotateToPlayer();
+        moveForward();
+        ShootPlayer();
     }
 }
--- Assets/Script/MelvinPlayerController.cs
+++ Assets/Script/MelvinPlayerController.cs
@@ -4,28 +4,52 @@ using UnityEngine;
 
 public class MelvinPlayerController : MonoBehaviour {
 
-    public float speed;
-    public float turnSpeed;
+    public float speed = 10f;
+    public float speedChange = 10f;
+    public float speedMax = 50f;
+    public float speedMin = -50f;
+    public float turnSpeed = 2f;
+    public float RotateSpeed = 1.5f;
+    public float fireRate = 0.5f;
     public GameObject shotPrefab;
 
-    void FixedUpdate() {
-        float current_speed = speed;
+    private float nextFire = 0.0f;
 
+    void movement()
+    {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
-            transform.Rotate(new Vector3(0, 0, 1), turnSpeed);
+            transform.Rotate(new Vector3(0, 0, 1), RotateSpeed);
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            transform.Rotate(new Vector3(0, 0, -1), turnSpeed);
+            transform.Rotate(new Vector3(0, 0, -1), RotateSpeed);
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
             transform.Rotate(new Vector3(1, 0, 0), turnSpeed);
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
             transform.Rotate(new Vector3(-1, 0, 0), turnSpeed);
+    }
+
+    void changeSpeed()
+    {
         if (Input.GetKey(KeyCode.LeftShift))
-            current_speed *= 15;
-        if (Input.GetKey(KeyCode.Space))
+            speed += speedChange * Time.deltaTime;
+        else
+            speed -= speedChange * Time.deltaTime;
+        speed = (speed > speedMax ? speedMax : (speed < speedMin ? speedMin : speed));
+    }
+        
+    void shoot()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             GameObject go = GameObject.Instantiate(shotPrefab, transform.position, transform.rotation) as GameObject;
             GameObject.Destroy(go, 3f);
         }
-        GetComponent<Rigidbody>().velocity = transform.forward * current_speed;
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    void FixedUpdate() {
+        movement();
+        changeSpeed();
+        shoot();
     }
 }
