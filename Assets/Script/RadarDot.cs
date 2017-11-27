aff5a30f408c8557d5dcffa1750b943c4e30855b
vglpz <vglpz7@gmail.com>
2017-11-27 12:49:56 +0900

    Radar and Scene
    Each radar dot currently needs to be manually connected to the corresponding enemy object - I shold change this to make it automatic

---
+++ Assets/Script/RadarDot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadarDot : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject enemy;
+
+	private float radar_radius;
+
+	void Start () {
+		radar_radius = 0.375f * 200; 
+//		q = 0.375f * GetComponent<RectTransform> ().rect.height;
+	}
+	
+	void Update () {
+		Vector3 direction = enemy.transform.position - player.transform.position;
+
+
+		float rotation = Vector3.SignedAngle (player.transform.forward,
+			direction,
+			Vector3.Cross(player.transform.forward,direction));
+
+		float angle = Vector3.SignedAngle (player.transform.right, 
+			Vector3.ProjectOnPlane (direction, player.transform.forward), 
+			player.transform.forward);
+
+		float x = Mathf.Cos (Mathf.Deg2Rad * angle);
+		float y = Mathf.Sin (Mathf.Deg2Rad * angle);
+
+		float factor = radar_radius * rotation / 180.0f;
+
+		x *= factor;
+		y *= factor;
+
+		float closeness = 1000.0f / (1000.0f + direction.magnitude);
+
+		Image i = GetComponentInChildren<Image> ();
+
+		i.color = new Color (1.0f, 1.0f, 1.0f, closeness);
+
+		transform.localPosition = new Vector2 (x, y);
+	}
+}
