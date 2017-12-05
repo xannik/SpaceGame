bf1a38475a7825d81c834c702a5d996b26be8e6a
xannik <rasmus_lindy@hotmail.com>
2017-12-05 14:02:38 +0900

    switchCamera script
    now canon is following rotation with ship

--- Assets/Script/SwitchCamera.cs
+++ Assets/Script/SwitchCamera.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class SwitchCamera : MonoBehaviour {
 	public Camera currentCamera;
-
+    public GameObject gobject;
+    
 	public Camera spaceShipCamera;
 	public Camera gunCamera;
     public MouseCamera script;
-	// Use this for initialization
-	void Start () {
+
+    private Renderer rend;
+    public GameObject cannon;
+    public GameObject spaceship;
+    private Quaternion angle;
+    // Use this for initialization
+    void Start () {
 		spaceShipCamera.enabled = true;
 		gunCamera.enabled = false;
         script = GetComponent<MouseCamera>();
         script.enabled = false;
-
+        rend = gobject.GetComponent<Renderer>();
 		currentCamera = spaceShipCamera;
-
+        cannon.transform.rotation = Quaternion.Euler(spaceship.transform.rotation.x, spaceship.transform.rotation.y - 180, spaceship.transform.rotation.z);
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
+        angle = spaceship.transform.rotation;
+
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
-
+            rend.enabled = !rend.enabled;
             spaceShipCamera.enabled = !spaceShipCamera.enabled;
             gunCamera.enabled = !gunCamera.enabled;
+            //Cursor.visible = false;
+            if (gunCamera.enabled == true)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            cannon.transform.localRotation = Quaternion.Euler(angle.x, (angle.y + 180) % 360, angle.z);
+        
+            
             script.enabled = !script.enabled;
-
+            
 			currentCamera = gunCamera.enabled ? gunCamera : spaceShipCamera;
         }
        
