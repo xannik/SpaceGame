93ba7d011bb31c4ab1d5c23940fdea8a7c183081
Melvin MOHADEB <melvin.mohadeb@epitech.eu>
2017-12-07 22:19:44 +0900

    Planet

---
+++ Assets/Script/SolarSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour {
+
+    public int speed = 1;
+    public GameObject Sun = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(Sun.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
